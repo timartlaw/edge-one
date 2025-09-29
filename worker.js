ab5021264a95988f6b77123625fac5505643685c
Tim <timartlaw@users.noreply.github.com>
2025-09-30 00:34:29 +0800

    Create worker.js
    day 1

---
+++ worker.js
@@ -0,0 +1,17 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+  async fetch(request, env, ctx) {
+    // You can view your logs in the Observability dashboard
+    console.info({ message: 'K Worker received a request!' }); 
+    return new Response('David开始戒酒, Day: 1');
+  }
+};
