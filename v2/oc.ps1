9245c8df20be8464fa331311ec8424d80c15ba5d
Tim <timartlaw@users.noreply.github.com>
2026-01-27 17:28:12 +0800

    Add initial PowerShell script for Opencode setup

---
+++ v2/oc.ps1
@@ -0,0 +1,22 @@
+# Set the path to the Opencode binary
+# $OpencodeBin = "C:\Users\11605\timtrick\nvm\nodejs\opencode"
+
+# Optional: Set a separate config directory
+$env:OPENCODE_CONFIG_DIR = "$pwd\.opencode"
+$env:OPENCODE_CONFIG = "$pwd\.opencode\opencode.json"
+# $env:OPENCODE_AUTH_JSON = "$pwd\.opencode\auth.json"
+# $env:OPENCODE_CACHE = "$pwd\.opencode\cache"
+
+# CRUSH
+$env:TIMGPT_API_ENDPOINT = "https://integrate.api.nvidia.com/v1"
+$env:TIMGPT_API_KEY = "YOUR_KEY"
+$env:CRUSH_DISABLE_PROVIDER_AUTO_UPDATE=0
+$env:CATWALK_URL="https://gh-proxy.org/https://github.com/timartlaw/edge-one/blob/main"
+
+# CODEX
+# $env:TIMGPT_API_KEY = "YOUR_KEY"
+$env:CODEX_HOME = "$pwd\.codex"
+# $env:Path += ";C:\Users\11605\timtrick\Git\bin"
+
+# Execute the binary and pass all incoming script arguments ($args)
+# & $OpencodeBin @args
